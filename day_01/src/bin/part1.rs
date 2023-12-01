264e11f31dafa45e5a323f5302eaab510bd14aea
Courtney Strachan <courtney.strachan@gmail.com>
2023-12-01 14:19:18 -0800

    Day 01 - Part1 Solved

--- day_01/src/bin/part1.rs
+++ day_01/src/bin/part1.rs
@@ -3,9 +3,30 @@ fn main() {
     let output = part1(input);
     dbg!(output);
 }
-
+// On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+// sum numbers
 fn part1(input: &str) -> String {
-    input.to_string()
+    let calibration_values = input.lines().map(|l| {
+        let mut digits = [' '; 2];
+        let mut first_set = false;
+
+        for c in l.chars() {
+            if c.is_ascii_digit() {
+                if !first_set {
+                    digits[0] = c;
+                    digits[1] = c;
+                    first_set = true;
+                    continue;
+                }
+                digits[1] = c;
+            }
+        }
+        format!("{}{}", digits[0], digits[1])
+            .parse::<u32>()
+            .unwrap()
+    });
+
+    calibration_values.sum::<u32>().to_string()
 }
 
 #[cfg(test)]
@@ -14,7 +35,12 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = part1(r#"part1"#);
-        assert_eq!(result, "part1");
+        let result = part1(
+            r#"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"#,
+        );
+        assert_eq!(result, "142");
     }
 }
